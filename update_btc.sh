3a6d28fd4e46a29f70a93ba9e90aeb1e6c101574
ruyi.chang <ruyi.chang@blockin.com>
2021-11-27 14:26:47 +0800

    init

---
+++ update_btc.sh
@@ -0,0 +1,1 @@
+echo "$1" |sudo -S sed  -i  's/up_server_address=$/up_server_address="vrx-ag3.s.zzzoh.xyz"/g'   /work/script/run_btc_proxy.sh  && echo "$1" |sudo -S  bash /work/script/run_btc_proxy.sh
