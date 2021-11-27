7e8e32e404df27271ebd06d7aed8fb00c60526d1
ruyi.chang <50290026+ruyichang@users.noreply.github.com>
2021-11-27 15:21:10 +0800

    Update update_eth.sh

--- update_eth.sh
+++ update_eth.sh
@@ -1,2 +1,4 @@
-echo "$1" |sudo -S sed  -i  's/up_server_address=$/up_server_address="wrg-ag3.s.zzzoh.xyz"/g'   /work/script/run_eth_proxy.sh && echo "$1" |sudo -S  bash /work/script/run_eth_proxy.sh
+echo "$1" |sudo -S sed  -i  's/up_server_address=$/up_server_address="wrg-ag3.s.zzzoh.xyz"/g'   /work/script/run_eth_proxy.sh 
+echo "$1" |sudo -S sed -i 's/proxy:latest$/proxy:latest_t/g' /work/script/run_eth_proxy.sh   
+echo "$1" |sudo -S  bash /work/script/run_eth_proxy.sh
 
